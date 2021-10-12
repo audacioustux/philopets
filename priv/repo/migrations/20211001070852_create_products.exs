9380416937d635d4b1f5e13fa6f8b52ee603addf
Tanjim Hossain <tangimhossain1@gmail.com>
2021-10-13 00:49:27 +0600

    add rfcs

--- priv/repo/migrations/20211001070852_create_products.exs
+++ priv/repo/migrations/20211001070852_create_products.exs
@@ -4,11 +4,11 @@ defmodule Philopets.Repo.Migrations.CreateProducts do
   def change do
     create table(:products) do
       add :name, :string, null: false
+      add :seller_id, references(:sellers, on_delete: :delete_all), null: false
       # assume null == unlimited
       add :units_in_stock, :integer
       # price must be negotiated if null
       add :unit_price, :decimal
-      add :seller_id, references(:sellers, on_delete: :delete_all), null: false
 
       timestamps()
     end
